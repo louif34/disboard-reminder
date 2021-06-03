e6aedae237deb1fcb13c7f4bc34b0156e9365eb0
Louis <louif34@hotmail.com>
2021-06-03 18:34:32 -0400

    Deleted bad folder names

--- Model/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-
-const userSchema = mongoose.Schema({
-  tag: { type: String, require: true },
-  server: { type: String, require: true }
-});
-
-module.exports = mongoose.model('User', userSchema);
